e14cfc98cd2bbf502fe11a288b0da5eb351fa115
Alessio Colella <alessiocolella@Mac.homenet.telecomitalia.it>
2026-04-21 16:16:37 +0200

    style: color gradient h1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [inputSearch, setInputSearch] = useState("");
  
   return (
-    <> <div className="w-full p-2.5 md:w-full md:p-5">
+    <> <div className="w-full p-2.5 md:w-full md:p-5 ">
+      <h1 className="text-center m-4 rounded-2xl bg-linear-to-tr/shorter from-cyan-300 via-red-200 to-blue-500">Search APP</h1>
       <Form inputSearch={inputSearch} setInputSearch={setInputSearch}  />
       <Result inputSearch={inputSearch} />
 
